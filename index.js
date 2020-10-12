b69069932a87d53d0281aa133aa11217779e71b5
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-12 14:54:48 -0600

    added place holder functions as well as a switch with cases. added a terminate function that can be run from the main screen with a confirm.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function init() {
         name: "initialize",
         type: "list",
         message: "What would you like to do?",
-        choices: ["Add Department", "Add Role", "Add Employee", "View Departments", "View Roles", "View Employees", "Update Employee Role"]
+        choices: ["Add Department", "Add Role", "Add Employee", "View Departments", "View Roles", "View Employees", "Update Employee Role", "EXIT"]
     })
     .then(function(res) {
         switch (res.initialize) {
@@ -31,7 +31,81 @@ function init() {
                 addDepartment();
                 break;
             
-            // case "Add Role"
+            case "Add Role":
+                addRole();
+                break;
+
+            case "Add Employee":
+                addEmployee();
+                break;
+
+            case "View Departments":
+                viewDepartments();
+                break;
+            case "View Roles":
+                viewRoles();
+                break;
+
+            case "View Employees":
+                viewEmployees();
+                break;
+            
+            case "Update Employee Role":
+                updateEmployee();
+                break;
+
+            case "EXIT":
+                closeApp();
+                break;
+        }
+    })
+}
+
+function addDepartment() {
+    console.log("Hello from inside addDepartment()")
+    init();
+}
+
+function addRole() {
+    console.log("Hello from inside addRole()")
+    init();
+}
+
+function addEmployee() {
+    console.log("Hello from inside addEmployee()")
+    init();
+}
+
+function viewDepartments() {
+    console.log("Hello from inside viewDepartments()")
+    init();
+}
+
+function viewRoles() {
+    console.log("Hellow from inside viewRoles()")
+    init();
+}
+
+function viewEmployees() {
+    console.log("Hellow from inside viewEmployees()")
+    init();
+}
+
+function updateEmployee() {
+    console.log("Hello from inside updateEmployee()")
+    init();
+}
+
+function closeApp() {
+    inquirer.prompt({
+        name: "confirm",
+        type: "confirm",
+        message: "Are you sure you want to terminate this application?"
+    }).then(function(res) {
+        if (res.confirm === true) {
+            connection.end();
+        } else {
+            init();
         }
     })
 }
