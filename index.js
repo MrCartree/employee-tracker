2191d72badd11c0e156ec16d1ed02bdc60e23203
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-12 14:30:14 -0600

    added a passwords.js to hide password and ignored it in gitignore. started using inquirer and connected to mysql.

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-let mysql = require("mysql");
-let inquirer = require("inquirer");
-let consoleTable = require("console.table");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const password = require("./password");
 // const { inherits } = require("util");
 
-let connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localHost",
     port: 3306,
     user: "root",
-    password: "",
+    password: password,
     // database: "employeeDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    // init();
+    init();
 });
+
+function init() {
+    inquirer.prompt({
+        name: "initialize",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["Add Department", "Add Role", "Add Employee", "View Departments", "View Roles", "View Employees", "Update Employee Role"]
+    })
+    .then(function(res) {
+        switch (res.initialize) {
+            case "Add Department":
+                addDepartment();
+                break;
+            
+            // case "Add Role"
+        }
+    })
+}
