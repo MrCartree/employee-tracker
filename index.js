ebf81d817440d5bc01de29d09132603211fbc1c1
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-11 20:30:12 -0600

    created connection with mysql.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
 let consoleTable = require("console.table");
+// const { inherits } = require("util");
+
+let connection = mysql.createConnection({
+    host: "localHost",
+    port: 3306,
+    user: "root",
+    password: "Ih8p4ssw0rds!",
+    // database: "employeeDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    // init();
+});
