dfdb676d7cf7b6a3023653c430d1bcc82d834795
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-15 21:49:59 -0600

    altered the seeds Db to be more better

--- DB/seed.sql
+++ DB/seed.sql
@@ -1,8 +1,8 @@
 INSERT INTO department (name)
-VALUES ("HR"), ("Sales"), ("Engineering");
+VALUES ("Janitorial"), ("Sales"), ("Engineering");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("HR Manager", 8000.50, 1);
+VALUES ("Head Janitor", 80000, 1);
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Billium", "Williamson", 1);
